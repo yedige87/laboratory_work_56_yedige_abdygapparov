ee16d05f1838ead48241eade1249b0b66f8e6643
yedige87 <yedabdygapparov@gmail.com>
2023-02-28 18:46:18 +0600

    изменения в файле settings

--- market/settings.py
+++ market/settings.py
@@ -129,3 +129,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+USE_THOUSAND_SEPARATOR = True
+USE_L10N = True
