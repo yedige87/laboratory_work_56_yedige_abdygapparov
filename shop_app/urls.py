215e969749ae0507b87196c2bbb7f4d4b80a68bf
yedige87 <yedabdygapparov@gmail.com>
2023-02-28 18:13:57 +0600

    созданы страницы для удаления с подтверждением товара, и обработчики для них

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from shop_app.views.base import index_view
-from shop_app.views.products import product_view
-
+from shop_app.views.products import product_view, product_delete_view, confirm_delete
 
 urlpatterns = [
     path("", index_view, name='index'),
     path('products', index_view, name='index'),
     path('products/<int:pk>', product_view, name='product_view'),
-
+    path('product/<int:pk>/delete', product_delete_view, name='product_delete'),
+    path('product/delete/<int:pk>', confirm_delete, name='confirm_delete'),
 ]
--- shop_app/views/products.py
+++ shop_app/views/products.py
@@ -11,3 +11,15 @@ def products_view(request: WSGIRequest):
 def product_view(request: WSGIRequest, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, 'product_view.html', context={'choices': CategoryChoice.choices, 'product': product})
+
+
+def product_delete_view(request: WSGIRequest, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'delete.html', context=context)
+
+
+def confirm_delete(request: WSGIRequest, pk):
+    product = get_object_or_404(Product, pk=pk)
+    product.delete()
+    return redirect('index')
