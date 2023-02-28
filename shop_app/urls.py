d11a48d15109a5f570c761e8dc21d8d4f9cd2679
yedige87 <yedabdygapparov@gmail.com>
2023-02-28 18:10:00 +0600

    создана главная страница, страница просмотра товара по id, написаны обработчики для них

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 from shop_app.views.base import index_view
+from shop_app.views.products import product_view
 
 
 urlpatterns = [
     path("", index_view, name='index'),
     path('products', index_view, name='index'),
+    path('products/<int:pk>', product_view, name='product_view'),
+
 ]
--- shop_app/views/products.py
+++ shop_app/views/products.py
@@ -1,6 +1,13 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404, render
+
+from shop_app.models import CategoryChoice, Product
 
 
 def products_view(request: WSGIRequest):
     return redirect('index')
+
+
+def product_view(request: WSGIRequest, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_view.html', context={'choices': CategoryChoice.choices, 'product': product})
